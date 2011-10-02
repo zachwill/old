00bd6abc88a8cb4edc946933a4521737f590209f
Zach Williams <hey@zachwill.com>
2011-10-02 15:17:17 -0700

    Add preliminary slide ability

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,6 @@
 (function(window, $) {
 
+    // Lightbulb becomes yellow on scroll.
     $(window).scroll(function(e) {
         var light = $('.idea_light'),
             visibility = light.css('visibility');
@@ -7,6 +8,41 @@
         if (visibility == 'hidden') {
             light.css('visibility', 'visible');
         }
+    });
+
+
+    // Create slider.
+    $('.slides').slides({
+        height: 190,
+        navigation: false,
+        next: 'next',
+        pagination: false,
+        prev: 'prev',
+        slide: {
+            interval: 400,
+            browserWindow: false,
+            easing: ""
+		},
+        width: 305
+    });
+
+
+    // Create custom slider controls.
+    var controls = [$('.prev'), $('.next')];
+
+    $.each(controls, function(index, value) {
+        value.click(function(e) {
+            var direction = value.html().toLowerCase(),
+                slides = $('.slides');
+
+            e.preventDefault();
+
+            if (direction == 'next') {
+                slides.slides('next');
+            } else {
+                slides.slides('previous');
+            }
+        });
     });
 
 
